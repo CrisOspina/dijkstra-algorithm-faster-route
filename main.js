354f00160844c2a25cea9926f5058b85626b64cb
CrisOspina <crisospina523@gmail.com>
2022-06-04 21:50:12 -0500

    feat: implementation dijkstra lib

---
+++ main.js
@@ -0,0 +1,14 @@
+import Graph from 'node-dijkstra'
+import {nodes} from './graph.js'
+
+export function getFasterRoute({origin, destiny}) {
+  const graph = new Graph()
+
+  for (const [node, neighbor] of Object.entries(nodes)) {
+    graph.addNode(node, neighbor)
+  }
+
+  const route = graph.path(origin, destiny).join(',')
+
+  return route
+}
